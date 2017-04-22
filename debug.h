5914e3550d0fd6f66795c3a795fc437c28bce3cf
xiaoyur347 <xiaoyur347@gmail.com>
2017-04-22 17:25:50 +0800

    pretty print

--- debug.h
+++ debug.h
@@ -4,9 +4,9 @@
 #include <stdio.h>
 #include <libgen.h>
 
-#define LOG_DEBUG(format, ...) printf("[%s:%d][Debug]" format "\n", basename((char*)__FILE__), __LINE__, ##__VA_ARGS__)
-#define LOG_INFO(format, ...) printf("[%s:%d][Info]" format "\n", basename((char*)__FILE__), __LINE__, ##__VA_ARGS__)
-#define LOG_WARN(format, ...) printf("[%s:%d][Warn]" format "\n", basename((char*)__FILE__), __LINE__, ##__VA_ARGS__)
-#define LOG_ERROR(format, ...) printf("[%s:%d][Error]" format "\n", basename((char*)__FILE__), __LINE__, ##__VA_ARGS__)
+#define LOG_DEBUG(format, ...) printf("[%s][%s:%d][Debug]" format "\n", basename((char*)__FILE__), __PRETTY_FUNCTION__, __LINE__, ##__VA_ARGS__)
+#define LOG_INFO(format, ...) printf("[%s][%s:%d][Info]" format "\n", basename((char*)__FILE__), __PRETTY_FUNCTION__, __LINE__, ##__VA_ARGS__)
+#define LOG_WARN(format, ...) printf("[%s][%s:%d][Warn]" format "\n", basename((char*)__FILE__), __PRETTY_FUNCTION__, __LINE__, ##__VA_ARGS__)
+#define LOG_ERROR(format, ...) printf("[%s][%s:%d][Error]" format "\n", basename((char*)__FILE__), __PRETTY_FUNCTION__, __LINE__, ##__VA_ARGS__)
 
 #endif
