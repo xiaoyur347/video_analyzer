669c9282e1d3f96687a72b59542cc484a9c3134e
xiaoyur347 <xiaoyur347@gmail.com>
2017-04-23 16:07:21 +0800

    fix h264 high profile

---
+++ es.cpp
@@ -0,0 +1,246 @@
+#include "es.h"
+#include "debug.h"
+
+bool H264ES::NalHeader::Analyze(BitBuffer &bits)
+{
+	nalu_header = bits.GetByte(4);
+	if (nalu_header != 0x00000001)
+	{
+		LOG_ERROR("Error nalu_header %u", nalu_header);
+		return false;
+	}
+	forbidden_zero_bit = bits.GetOneBit();
+	nal_ref_idc = bits.GetBit(2);
+	nal_unit_type = bits.GetBit(5);
+
+	LOG_INFO("nal_ref_idc=%u,nal_unit_type=%u %s", nal_ref_idc, nal_unit_type, GetName());
+	return true;
+}
+
+const char *H264ES::NalHeader::GetName() const
+{
+	switch (nal_unit_type)
+	{
+		case 1:
+			return "slice_layer_without_partitioning_rbsp";
+		case 2:
+			return "[I_Slice]slice_data_partition_a_layer_rbsp";
+		case 3:
+			return "[P_Slice]slice_data_partition_b_layer_rbsp";
+		case 4:
+			return "[B_Slice]slice_data_partition_c_layer_rbsp";
+		case 5:
+			return "[IDR]slice_layer_without_partitioning_rbsp";
+		case 6:
+			return "sei_rbsp";
+		case 7:
+			return "[SPS]seq_parameter_set_rbsp";
+		case 8:
+			return "[PPS]pic_parameter_set_rbsp";
+		case 9:
+			return "access_unit_delimiter_rbsp";
+		case 10:
+			return "end_of_seq_rbsp";
+		case 11:
+			return "end_of_stream_rbsp";
+		case 12:
+			return "filler_data_rbsp";
+		case 13:
+			return "seq_parameter_set_extension_rbsp";
+		case 14:
+		case 15:
+		case 16:
+		case 17:
+		case 18:
+			return "reserved";
+		case 19:
+			return "slice_layer_without_partitioning_rbsp";
+		case 20:
+		case 21:
+		case 22:
+		case 23:
+			return "reserved";
+		default:
+			return "undefined";
+	}
+	return "undefined";
+}
+
+bool H264ES::SPS::Analyze(BitBuffer &bits)
+{
+	profile_idc = bits.GetByte(1);
+	constraint_set0_flag = bits.GetOneBit();
+	constraint_set1_flag = bits.GetOneBit();
+	constraint_set2_flag = bits.GetOneBit();
+	constraint_set3_flag = bits.GetOneBit();
+	reserved_zero_4bits = bits.GetBit(4);
+	level_idc = bits.GetByte(1);
+
+	seq_parameter_set_id = bits.GetUEV();
+
+	if (profile_idc == 100) //high
+	{
+		chroma_format_idc = bits.GetUEV();
+		bit_depth_luma_minus8 = bits.GetUEV();
+		bit_depth_chroma_minus8 = bits.GetUEV();
+		qpprime_y_zero_transform_bypass_flag = bits.GetOneBit();
+		seq_scaling_matrix_present_flag = bits.GetOneBit();
+	}
+
+	log2_max_frame_num_minus4 = bits.GetUEV();
+	pic_order_cnt_type = bits.GetUEV();
+
+	if (pic_order_cnt_type == 0)
+	{
+		log2_max_pic_order_cnt_lsb_minus4 = bits.GetUEV();
+	}
+	else if (pic_order_cnt_type == 1)
+	{
+		delta_pic_order_always_zero_flag = bits.GetOneBit();
+		offset_for_non_ref_pic = bits.GetSEV();
+		offset_for_top_to_bottom_field = bits.GetSEV();
+		num_ref_frames_in_pic_order_cnt_cycle = bits.GetUEV();
+		for (unsigned i = 0; i < num_ref_frames_in_pic_order_cnt_cycle; i++)
+		{
+			offset_for_ref_frame.push_back((signed char)bits.GetSEV());
+		}
+	}
+	num_ref_frames = bits.GetUEV();
+	gaps_in_frame_num_value_allowed_flag = bits.GetOneBit();
+	pic_width_in_mbs_minus1 = bits.GetUEV();
+	pic_height_in_map_units_minus1 = bits.GetUEV();
+	frame_mbs_only_flag = bits.GetOneBit();
+	if (!frame_mbs_only_flag)
+	{
+		mb_adaptive_frame_field_flag = bits.GetOneBit();
+	}
+	direct_8x8_inference_flag = bits.GetOneBit();
+	frame_cropping_flag = bits.GetOneBit();
+	if (frame_cropping_flag)
+	{
+		frame_crop_left_offset = bits.GetUEV();
+		frame_crop_right_offset = bits.GetUEV();
+		frame_crop_top_offset = bits.GetUEV();
+		frame_crop_bottom_offset = bits.GetUEV();
+	}
+	vui_parameters_present_flag = bits.GetOneBit();
+
+	LOG_ERROR("profile=%u,level=%u,num_ref_frames=%u,width=%u,height=%u",
+		profile_idc, level_idc,
+		num_ref_frames,
+		(pic_width_in_mbs_minus1 + 1) * 16,
+		(pic_height_in_map_units_minus1 + 1) * 16);
+	return true;
+}
+
+void H264ES::JumpToNaluHeader(BitBuffer &bits)
+{
+	bits.SkipTrailing();
+	bits.Dump();
+	while (!bits.IsEmpty())
+	{
+		unsigned temp = bits.GetByte(1);
+		if (temp != 0)
+		{
+			continue;
+		}
+		temp = bits.GetByte(1);
+		if (temp != 0)
+		{
+			continue;
+		}
+		temp = bits.GetByte(1);
+		if (temp != 0)
+		{
+			continue;
+		}
+		temp = bits.GetByte(1);
+		if (temp != 1)
+		{
+			continue;
+		}
+		bits.RewindBit(32);
+		LOG_INFO("FIND NALU HEADER");
+		return;
+	}
+}
+
+bool H264ES::Analyze(BitBuffer &bits)
+{
+	NalHeader header;
+	if (!header.Analyze(bits))
+	{
+		return false;
+	}
+	if (header.nal_unit_type == 9)
+	{
+		bits.SkipByte(1);
+		return true;
+	}
+	else if (header.nal_unit_type == 7)
+	{
+		//SPS
+		SPS sps;
+		sps.Analyze(bits);
+		JumpToNaluHeader(bits);
+	}
+	else if (header.nal_unit_type == 8)
+	{
+		//PPS
+		JumpToNaluHeader(bits);
+	}
+	else if (header.nal_unit_type == 6)
+	{
+		//SEI
+		JumpToNaluHeader(bits);
+	}
+	else if (header.nal_unit_type == 5)
+	{
+		//IDR
+		JumpToNaluHeader(bits);
+	}
+	else if (header.nal_unit_type == 1)
+	{
+		JumpToNaluHeader(bits);
+	}
+	return true;
+}
+
+bool MPEGAudioES::PacketHeader::Analyze(BitBuffer &bits)
+{
+	sync_word = bits.GetBit(12);
+	if (sync_word != 0xfff)
+	{
+		LOG_ERROR("Error sync_word %u", sync_word);
+		return false;
+	}
+	ID = bits.GetOneBit();
+	layer = bits.GetBit(2);
+
+	no_protection = bits.GetOneBit();
+	bit_rate_index = bits.GetBit(4);
+	sampling_frequency = bits.GetBit(2);
+	padding = bits.GetOneBit();
+	privatea = bits.GetOneBit();
+	mode = bits.GetBit(2);
+	mode_extension = bits.GetBit(2);
+	copyright = bits.GetOneBit();
+	original_or_copy = bits.GetOneBit();
+	emphasis = bits.GetBit(2);
+
+	LOG_WARN("ID=%s,layer=%u,sampling_frequency=%s kHz",
+		ID == 1 ? "mpeg1" : "mpeg2",
+		4 - layer,
+	 	sampling_frequency == 0 ? "44.1" : sampling_frequency == 1 ? "48" : "32");
+	return true;
+}
+
+bool MPEGAudioES::Analyze(BitBuffer &bits)
+{
+	PacketHeader header;
+	if (!header.Analyze(bits))
+	{
+		return false;
+	}
+	return true;
+}
---
+++ es.h
@@ -0,0 +1,168 @@
+#ifndef ES_H
+#define ES_H
+
+#include "buffer.h"
+#include <vector>
+
+class H264ES
+{
+public:
+	struct NalHeader
+	{
+		unsigned nalu_header : 32;
+		unsigned forbidden_zero_bit : 1;
+		unsigned nal_ref_idc : 2;
+		unsigned nal_unit_type : 5;
+
+		NalHeader()
+			:nalu_header(0),
+			forbidden_zero_bit(0),
+			nal_ref_idc(0),
+			nal_unit_type(0)
+		{
+		}
+		bool Analyze(BitBuffer &bits);
+		const char *GetName() const;
+	};
+	struct VUI
+	{
+		unsigned aspect_ratio_info_present_flag : 1;
+		unsigned aspect_ratio_idc : 8;
+		unsigned sar_width : 16;
+		unsigned sar_height : 16;
+
+		unsigned overscan_info_present_flag : 1;
+		//unsigned
+	};
+	struct SPS //H264 7.3.2.1
+	{
+		unsigned profile_idc : 8;
+		unsigned constraint_set0_flag : 1;
+		unsigned constraint_set1_flag : 1;
+		unsigned constraint_set2_flag : 1;
+		unsigned constraint_set3_flag : 1;
+		unsigned reserved_zero_4bits : 4;
+		unsigned level_idc : 8;
+
+		//and more
+		unsigned seq_parameter_set_id;
+
+		//only in high
+		unsigned chroma_format_idc;
+		unsigned bit_depth_luma_minus8;
+		unsigned bit_depth_chroma_minus8;
+		unsigned qpprime_y_zero_transform_bypass_flag : 1;
+		unsigned seq_scaling_matrix_present_flag : 1;
+
+		unsigned log2_max_frame_num_minus4;
+		unsigned pic_order_cnt_type;
+		unsigned log2_max_pic_order_cnt_lsb_minus4;
+
+		unsigned delta_pic_order_always_zero_flag : 1;
+		int offset_for_non_ref_pic;
+		int offset_for_top_to_bottom_field;
+		unsigned num_ref_frames_in_pic_order_cnt_cycle;
+		std::vector<signed char> offset_for_ref_frame;
+
+		unsigned num_ref_frames;
+		unsigned gaps_in_frame_num_value_allowed_flag : 1;
+		unsigned pic_width_in_mbs_minus1;
+		unsigned pic_height_in_map_units_minus1;
+		unsigned frame_mbs_only_flag : 1;
+		unsigned mb_adaptive_frame_field_flag : 1;
+		unsigned direct_8x8_inference_flag : 1;
+		unsigned frame_cropping_flag : 1;
+
+		unsigned frame_crop_left_offset;
+		unsigned frame_crop_right_offset;
+		unsigned frame_crop_top_offset;
+		unsigned frame_crop_bottom_offset;
+
+		unsigned vui_parameters_present_flag : 1;
+
+		SPS()
+			:profile_idc(0),
+			constraint_set0_flag(0),
+			constraint_set1_flag(0),
+			constraint_set2_flag(0),
+			constraint_set3_flag(0),
+			reserved_zero_4bits(0),
+			level_idc(0),
+			seq_parameter_set_id(0),
+			chroma_format_idc(0),
+			bit_depth_luma_minus8(0),
+			bit_depth_chroma_minus8(0),
+			qpprime_y_zero_transform_bypass_flag(0),
+			seq_scaling_matrix_present_flag(0),
+			log2_max_frame_num_minus4(0),
+			pic_order_cnt_type(0),
+			log2_max_pic_order_cnt_lsb_minus4(0),
+			delta_pic_order_always_zero_flag(0),
+			offset_for_non_ref_pic(0),
+			offset_for_top_to_bottom_field(0),
+			num_ref_frames_in_pic_order_cnt_cycle(0),
+			num_ref_frames(0),
+			gaps_in_frame_num_value_allowed_flag(0),
+			pic_width_in_mbs_minus1(0),
+			pic_height_in_map_units_minus1(0),
+			frame_mbs_only_flag(0),
+			mb_adaptive_frame_field_flag(0),
+			direct_8x8_inference_flag(0),
+			frame_cropping_flag(0),
+			frame_crop_left_offset(0),
+			frame_crop_right_offset(0),
+			frame_crop_top_offset(0),
+			frame_crop_bottom_offset(0),
+			vui_parameters_present_flag(0)
+		{
+
+		}
+		bool Analyze(BitBuffer &bits);
+	};
+	bool Analyze(BitBuffer &bits);
+	void JumpToNaluHeader(BitBuffer &bits);
+};
+
+class MPEGAudioES
+{
+public:
+	struct PacketHeader
+	{
+		unsigned sync_word : 12; //0xfff
+		unsigned ID : 1; //'1'=mpeg1 '0'=mpeg2
+		unsigned layer : 2; //'11'=1 '10'=2 '01'=3
+		unsigned no_protection : 1;
+		unsigned bit_rate_index : 4;
+		unsigned sampling_frequency : 2; //kHz '00'=44.1 '01'=48 '10'=32
+		unsigned padding : 1;
+		unsigned privatea : 1;
+		unsigned mode : 2; //'00'=Stereo '01'=joint stereo '10'=dual channel '11'=single channel
+		unsigned mode_extension : 2;
+		unsigned copyright : 1; //0=none 1=yes
+		unsigned original_or_copy : 1; //0=copy 1=original
+		unsigned emphasis : 2;
+
+		PacketHeader()
+			:sync_word(0),
+			 ID(0),
+			 layer(0),
+			 no_protection(0),
+			 bit_rate_index(0),
+			 sampling_frequency(0),
+			 padding(0),
+			 privatea(0),
+			 mode(0),
+			 mode_extension(0),
+			 copyright(0),
+			 original_or_copy(0),
+			 emphasis(0)
+		{
+
+		}
+		bool Analyze(BitBuffer &bits);
+	};
+
+	bool Analyze(BitBuffer &bits);
+};
+
+#endif
